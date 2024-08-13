c7f12f2f6c18aebcf28190b5d111cb047ca2897a
Jean-Baptiste Kaiser <jb@marmelab.com>
2024-08-13 11:59:56 +0200

    fix makefile and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 .PHONY: build dev
 
 build:
-    npm run build
+	npm run build
 
 dev:
-    npm run dev
+	npm run dev
 
 lint:
-    npm run lint
+	npm run lint
